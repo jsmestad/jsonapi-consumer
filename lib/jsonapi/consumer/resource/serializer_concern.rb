4cfdc77d635d1295f82c36fb005a23c39f77121e
Justin Smestad <justin.smestad@gmail.com>
2015-01-23 12:29:11 -0700

    Add internal helper #remove_links? for association payloads

--- lib/jsonapi/consumer/resource/serializer_concern.rb
+++ lib/jsonapi/consumer/resource/serializer_concern.rb
@@ -8,12 +8,12 @@ module JSONAPI::Consumer::Resource
       self.each_association do |name, association, options|
         @hash[:links] ||= {}
 
-        if association.respond_to?(:each)
+        if association.respond_to?(:each) or _association_type(name) == :has_many
           add_links(name, association, options)
         else
           add_link(name, association, options)
         end
-        @hash.delete(:links) if @hash[:links].length == 0 && !persisted?
+        @hash.delete(:links) if remove_links?
       end
 
       @hash
@@ -21,7 +21,7 @@ module JSONAPI::Consumer::Resource
 
     def add_links(name, association, options)
       @hash[:links][name] ||= []
-      @hash[:links][name] += association.map do |obj|
+      @hash[:links][name] += (association || []).map do |obj|
         case obj.class
         when String, Integer
           obj
@@ -44,6 +44,20 @@ module JSONAPI::Consumer::Resource
 
     def to_json(options={})
       serializable_hash(options).to_json
+    end
+
+  private
+
+    def remove_links?
+      if persisted?
+        false
+      else # not persisted, new object
+        if @hash[:links].length == 0 or @hash[:links].values.flatten.empty?
+          true
+        else
+          false
+        end
+      end
     end
   end
 end
